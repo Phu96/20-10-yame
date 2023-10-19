33e145707d4aeea3c7b4936d1bbf5586e1055ff7
visca <viscafcb96@gmail.com>
2023-10-19 20:36:30 +0700

    init

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hi Yame, today is a special day. </br> I have a gift for u!",
+  descriptionHeader:
+    "Do u wanna receive my gift?",
+  buttonYes: "Yes",
+  buttonNo: "No",
+  titleModar: "Thanks u!.",
+  descriptionModar:
+    "Happy Vietnamese Women’s Day to the one who has stolen my heart!. </br> I wish you a peaceful life, be happy, and be strong as you always are.</br> Love you <3",
+};
